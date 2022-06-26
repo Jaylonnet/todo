ec961731096bae96070f4f9051916bce832f5fb2
Jaylon <jaylonwingfield10@gmail.com>
2022-06-26 16:10:14 +0100

    created storageManager object

---
+++ src/storageManager.js
@@ -0,0 +1,15 @@
+export const storageManager = () => {
+    const getProjects = () => {
+        const projects = localStorage.getItem("projects");
+        if (projects === null) {
+            return null;
+        } else {
+            return JSON.parse(projects);
+        };
+    };
+
+    const writeProjects = () => {
+        
+    };
+    return {getProjects}
+};
