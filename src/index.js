c8fb6277c45c6bee27c353682631e99265fc3dc8
Jaylon <jaylonwingfield10@gmail.com>
2022-06-28 19:22:06 +0100

    re-arrange code, and write code to create default project

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ const projectMgr = projectManager();
 const initProjects = function (){
     const items = storage.getProjects();
 
-    if (items === null) {
-        
-    };
-    
     const createDefaultProject = () => {
         const defaultTask = task('Task 1', 'Description', 'Priority', 'Due Date');
         const defaultProject = project('Project 1');
         defaultProject.addTask(defaultTask);
         return defaultProject;
     };
+
+    if (items === null) {
+        projectMgr.projects.push(createDefaultProject());
+        storage.writeProjects(projectMgr.projects);
+    };
+
 }();
