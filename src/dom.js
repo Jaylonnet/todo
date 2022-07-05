a23e6ffa3b31788c07f8d5460a2ff01dc23964d8
Jaylon <jaylonwingfield10@gmail.com>
2022-07-05 19:51:02 +0100

    displayProjectOnPage function created

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,6 @@
-export function createProjectInDOM() {
-    
+export function displayProjectOnPage(project, projectIdentifier) {
+    const heading = document.createElement('h3');
+    heading.textContent = project.title;
+    heading.dataset.projectIdentifier = projectIdentifier;
+    document.querySelector('#project-container').append(heading);
 };
