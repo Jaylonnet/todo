fc1473bf6394ebb5dc9d2d49f35bc56a39980030
Jaylon <jaylonwingfield10@gmail.com>
2022-07-05 20:08:57 +0100

    addListenerToProjectHeading function created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { storageManager } from "./storage.js";
 import { displayProjectOnPage } from "./dom.js";
 
 
+let selectedProject = 0;
+
 const createProjectBtn = document.querySelector('#createProjectBtn');
 createProjectBtn.addEventListener('click', () => {
     const form = document.forms.createProjectForm;
@@ -13,4 +15,12 @@ function createNewProject(title) {
     const newProject = project(title, projectManager.generateProjectIdentifier);
     projectManager.addProject(newProject);
     displayProjectOnPage(newProject, newProject.getProjectIdentifier);
+    addListenerToProjectHeading(newProject.getProjectIdentifier);
 };
+
+function addListenerToProjectHeading(datasetId) {
+    const projectHeading = document.querySelector(`h3[data-project-identifier="${datasetId}"]`);
+    projectHeading.addEventListener('click', (e) => {
+
+    });
+}
