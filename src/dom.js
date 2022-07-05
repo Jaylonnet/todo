cdd5ebed81ea660c73482b9ae004548eab130121
Jaylon <jaylonwingfield10@gmail.com>
2022-07-05 16:26:13 +0100

    revamp

--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,3 @@
-export function DOMCreateProject(project, index, node) {
-    const ele = document.createElement('span');
-    ele.textContent = project.title;
-    ele.dataset.projectIndex = index;
-    node.appendChild(ele)
+export function createProjectInDOM() {
+    
 };
