b7c507ec127d852db45c414e31fbeead94fa6def
Jaylon <jaylonwingfield10@gmail.com>
2022-07-05 17:44:51 +0100

    added btn event listener and created createNewProject function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import { task, project, projectManager } from "./toDo.js";
 import { storageManager } from "./storage.js";
 import { displayProjectOnPage } from "./dom.js";
 
+
+const createProjectBtn = document.querySelector('#createProjectBtn');
+createProjectBtn.addEventListener('click', () => {
+    const form = document.forms.createProjectForm;
+    createNewProject(form.projectTitle.value);
+});
+
+function createNewProject(title) {
+    const newProject = project(title, projectManager.generateProjectIdentifier);
+    projectManager.addProject(newProject);
+    console.log(newProject.getProjectIdentifier)
+};
