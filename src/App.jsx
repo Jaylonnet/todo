53121930f56ba4494065feabab750be33024d3b6
Jaylonnet <jaylonwingfield10@gmail.com>
2025-07-13 17:29:44 +0100

    feat: implement task creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import FilterButton from "./components/FilterButton";
+import CheckIcon from "./components/CheckIcon";
+import CrossIcon from "./components/CrossIcon";
 
 const FILTER_MAP = {
   All: () => true,
@@ -12,17 +14,22 @@ const FILTER_NAMES = Object.keys(FILTER_MAP);
 function App({ taskData }) {
   const [filter, setFilter] = useState("All");
   const [taskList, setTaskList] = useState([...taskData]);
+  const [isCreating, setIsCreating] = useState(false);
+  const [taskText, setTaskText] = useState("");
 
   const clearCompleted = () => {
     const remainingTasks = taskList.filter((task) => !task.completed);
     setTaskList(remainingTasks);
   };
 
-  const tasks = taskList.filter(FILTER_MAP[filter]).map((task) => (
-    <li className="task-item" key={task.id}>
-      {task.text}
-    </li>
-  ));
+  const tasks = taskList
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <li className="task-item" key={task.id}>
+        {task.text}
+      </li>
+    ))
+    .reverse();
 
   const filterList = FILTER_NAMES.map((name) => (
     <FilterButton
@@ -32,6 +39,17 @@ function App({ taskData }) {
       setFilter={setFilter}
     />
   ));
+
+  const addTask = (taskName) => {
+    if (!taskName) return;
+    setTaskList([
+      { id: crypto.randomUUID(), text: taskName, completed: false },
+      ...taskList,
+    ]);
+    setTaskText("");
+    setIsCreating(false);
+  };
+
   return (
     <div className="content">
       <header className="header">
@@ -40,7 +58,43 @@ function App({ taskData }) {
       <main>
         <section className="task-section">
           <div className="create-task">
-            <button className="create-task__btn">Create a new todo...</button>
+            {!isCreating ? (
+              <button
+                className="create-task__btn"
+                onClick={() => setIsCreating(!isCreating)}
+              >
+                Create a new todo...
+              </button>
+            ) : (
+              <>
+                <input
+                  onKeyDown={(e) =>
+                    e.key === "Enter" ? addTask(taskText) : null
+                  }
+                  className="new-task-input"
+                  autoFocus
+                  onChange={(e) => {
+                    setTaskText(e.target.value);
+                  }}
+                  value={taskText}
+                ></input>
+                <button
+                  className="submit-task"
+                  onClick={() => addTask(taskText)}
+                >
+                  <CheckIcon fillColor="hsl(233, 14%, 35%)" />
+                </button>
+                <button
+                  className="cancel-task"
+                  onClick={() => {
+                    setIsCreating(false);
+                    setTaskText("");
+                  }}
+                >
+                  <CrossIcon fillColor="hsl(233, 14%, 35%)" />
+                </button>
+              </>
+            )}
           </div>
           <ul className="task-list">{tasks}</ul>
           <div className="task-count">
---
+++ src/components/CheckIcon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CheckIcon = ({ fillColor }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="20"
+      className="check-icon"
+      viewBox="0 0 9 9"
+    >
+      <path
+        fill="none"
+        stroke={fillColor}
+        stroke-width="2"
+        d="M1 4.304L3.696 7l6-6"
+      />
+    </svg>
+  );
+};
+
+export default CheckIcon;
